a71918cdec55a13c80d717ed3475e72a47c6ed0d
Arham Mirkar <arhammirker1@gmail.com>
2025-09-21 06:35:32 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    port: 3000
   },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    __API_URL__: JSON.stringify('https://fatooraah.com/api')
+  },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
